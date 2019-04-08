899d5d8437a3e6fc38ab9cdf718618df014aca1a
Jonaluna <48217920+Jonaluna@users.noreply.github.com>
2019-04-08 12:12:58 -0500

    Add files via upload

---
+++ tablero.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto
+{
+    public partial class Tablero : Form
+    {
+        public Tablero()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
